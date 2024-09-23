cb91a52ca3991efac147544d8745ccf5fe517e9b
reinardricky <reinardricky@gmail.com>
2024-09-23 10:48:14 +0700

    Added Dockerfile

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,11 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
+        url: configService.get<string>('DATABASE_URL'),
         entities: [User, Task],
         synchronize: true,
       }),
--- backend/src/auth/jwt-payload.interface.ts
+++ backend/src/auth/jwt-payload.interface.ts
@@ -1,6 +1,5 @@
 export interface JwtPayload {
-    sub: number;   // 'sub' is a standard claim in JWT, often representing the user's ID
-    email: string; // Email is another common claim
-    roles: string[];  // Roles will be used to protect routes
-  }
-  
+  sub: number;
+  email: string;
+  roles: string[]; // Roles will be used to protect routes
+}
